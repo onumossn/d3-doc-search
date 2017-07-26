64be902a3cc4db7305c26162da48c2e21884b9ec
Mohammed Nauage <mohammed.nauage.gsa@gmail.com>
2017-07-26 04:17:39 -0500

    update angular and fix body margin for in user-stylesheets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot()
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   bootstrap: [AppComponent]
 })
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import './polyfills.ts';
+import 'hammerjs';
+import './polyfills';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
